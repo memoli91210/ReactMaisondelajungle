ce53bde79c8bcc985f875e40f5615ae73dbfe140
memoli91210 <76181778+memoli91210@users.noreply.github.com>
2022-02-17 15:12:13 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Banner from "./Coponents/Banner";
+import Cart from "./Coponents/Cart";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
